064d1b9161d90f16d2a04a4317a663bddd32925b
unknown <wqgs200421@163.com>
2013-06-07 00:07:16 +0800

    db

--- src/main/java/smt/middleware/database/DBConnect.java
+++ src/main/java/smt/middleware/database/DBConnect.java
@@ -1,47 +1,163 @@
 package smt.middleware.database;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Dictionary;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import org.apache.log4j.Logger;
 
 import smt.middleware.entity.DataTable;
 
 public abstract class DBConnect {
 
-	public DBConnect() { }
+	private final Logger log = Logger.getLogger(DBConnect.class);
+	protected Connection connection;
 
-    //填充dataset
-    public abstract String deleteBySql(String strsql);
-    //提交删除命令 
-    public abstract String updateBySql(String strsql);//提交更新命令   
-    public abstract String insertBySql(String strsql);//提交插入命令
+	public DBConnect(Connection connection) {
+		this.connection = connection;
+	}
 
-    /// <summary>
-    /// 根据IDE的查询分析器的sql语句获取数据
-    /// </summary>
-    /// <param name="sql">sql语句</param>
-    /// <returns>返回数据集</returns>
-    public abstract DataTable getQuery(String sql);
-    
-    /// <summary>
-    /// 根据IDE的查询分析器的sql语句操作数据(insert/update/delete)
-    /// </summary>
-    /// <param name="sql">sql语句</param>
-    /// <returns>返回int</returns>
-    public abstract int dmlSql(String sql);
-    
-    /// <summary>
-    /// 根据IDE的查询分析器的sql语句获取数据
-    /// </summary>
-    /// <param name="sql">sql语句</param>
-    ///  <param name="sql">参数</param>
-    /// <returns>返回数据集</returns>
-    public abstract DataTable getQuery(String sql,Map<String,String> sqlParam);
+	/**
+	 * 提交删除命令
+	 */
+	public String deleteBySql(String strsql) {
+		// TODO Auto-generated method stub
+		PreparedStatement pre = null;
+		try {
+			pre = connection.prepareStatement(strsql);
+			pre.executeUpdate();
+			return "ok";
+		} catch (SQLException e) {
+			return e.getMessage();
+		} finally {
+			DBConnection.close(connection, pre, null);
+		}
+	}
 
-    /// <summary>
-    /// 根据IDE的查询分析器的sql语句操作数据(insert/update/delete)
-    /// </summary>
-    /// <param name="sql">sql语句</param>
-    /// <param name="sqlParam">参数</param>
-    /// <returns>返回int</returns>
-    public abstract int dmlSql(String sql,Map<String,String> sqlParam);
+	/**
+	 * 提交更新命令
+	 */
+	public String updateBySql(String strsql) {
+		PreparedStatement pre = null;
+		try {
+			pre = connection.prepareStatement(strsql);
+			pre.executeUpdate();
+			return "ok";
+		} catch (SQLException e) {
+			return e.getMessage();
+		} finally {
+			DBConnection.close(connection, pre, null);
+		}
+	}
+
+	public String insertBySql(String strsql) {
+		PreparedStatement pre = null;
+		try {
+			pre = connection.prepareStatement(strsql);
+			pre.executeUpdate();
+			return "ok";
+		} catch (SQLException e) {
+			return e.getMessage();
+		} finally {
+			DBConnection.close(connection, pre, null);
+		}
+	}
+
+	public DataTable getQuery(String sql) {
+		String strSql = sql;
+		DataTable dataTable = null;
+		PreparedStatement pre = null;
+		ResultSet rs = null;
+		try {
+			pre = connection.prepareStatement(strSql);
+			rs = pre.executeQuery();
+			dataTable = new DataTable(connection,pre,rs);
+			return dataTable;
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} finally {
+			DBConnection.close(connection, pre, rs);
+		}
+		return dataTable;
+	}
+
+	public int dmlSql(String sql) {
+		PreparedStatement pre = null;
+		int result = 0;
+		try {
+			pre = connection.prepareStatement(sql);
+			result = pre.executeUpdate();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} finally {
+			DBConnection.close(connection, pre, null);
+		}
+		return result;
+	}
+
+	public DataTable getQuery(String sql, Map<String, String> sqlParam) {
+		PreparedStatement pre = null;
+		ResultSet rs = null;
+		try{
+			Set<Entry<String, String>> set = sqlParam.entrySet();
+			Iterator<Entry<String, String>> iterator = set.iterator();
+			List<String> list = new ArrayList<String>();
+			for (Iterator iterator2 = set.iterator(); iterator2.hasNext();) {
+				Entry<String, String> entry = (Entry<String, String>) iterator2
+						.next();
+				String key = entry.getKey();
+				String value = entry.getValue();
+				sql = sql.replaceAll(key, "?");
+				list.add(value);
+			}
+			pre = connection.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+			log.debug(sql);
+			log.debug(list.toString());
+			for (String str : list) {
+				pre.setObject(list.indexOf(str) + 1, str);
+			}
+			return new DataTable(connection,pre,pre.executeQuery());
+		}catch(SQLException ex){
+			log.error("Query Erro", ex);
+		}finally{
+			//DBConnection.close(connection, pre, rs);
+		}
+		return null;
+	}
+
+	public int dmlSql(String sql, Map<String, String> sqlParam) {
+		PreparedStatement pre = null;
+		int count = 0;
+		try{
+			Set<Entry<String, String>> set = sqlParam.entrySet();
+			Iterator<Entry<String, String>> iterator = set.iterator();
+			List<String> list = new ArrayList<String>();
+			for (Iterator iterator2 = set.iterator(); iterator2.hasNext();) {
+				Entry<String, String> entry = (Entry<String, String>) iterator2
+						.next();
+				String key = entry.getKey();
+				String value = entry.getValue();
+				sql = sql.replaceAll(key, "?");
+				list.add(value);
+			}
+			pre = connection.prepareStatement(sql);
+			for (String str : list) {
+				pre.setObject(list.indexOf(str) + 1, str);
+			}
+			count = pre.executeUpdate();
+		}catch(SQLException ex){
+			log.error("Query Erro", ex);
+		}finally{
+			DBConnection.close(connection, pre, null);
+		}
+		return count;
+	}
 }
---
+++ src/main/java/smt/middleware/database/DBMysql.java
@@ -0,0 +1,12 @@
+package smt.middleware.database;
+
+import java.sql.Connection;
+
+public class DBMysql extends DBConnect{
+
+	public DBMysql(Connection connection) {
+		super(connection);
+		// TODO Auto-generated constructor stub
+	}
+
+}
